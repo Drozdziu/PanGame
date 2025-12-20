ca42d566e65cf4962911f0c6fde28bb6292e2435
Drozdziu <kubawanat625@gmail.com>
2025-12-20 19:15:20 +0100

    Playable game
    Added bots, sorting card decks

--- src/App.vue
+++ src/App.vue
@@ -1,49 +1,129 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue';
+import cardsDeckJSON from './cardsDeck.json'
 import CardComp from './components/CardComp.vue';
 interface card {
   name: string,
   value: number,
 };
-const cardsDeck: card[] = [{ name: 'AceOfHearts', value: 5 }, { name: 'AceOfDiamonds', value: 5 }, { name: 'AceOfSpades', value: 5 }, { name: 'AceOfClubs', value: 5 },
-{ name: 'KingOfHearts', value: 4 }, { name: 'KingOfDiamonds', value: 4 }, { name: 'KingOfSpades', value: 4 }, { name: 'KingOfClubs', value: 4 },
-{ name: 'QueenOfHearts', value: 3 }, { name: 'QueenOfDiamonds', value: 3 }, { name: 'QueenOfSpades', value: 3 }, { name: 'QueenOfClubs', value: 3 },
-{ name: 'JackOfHearts', value: 2 }, { name: 'JackOfDiamonds', value: 2 }, { name: 'JackOfSpades', value: 2 }, { name: 'JackOfClubs', value: 2 },
-{ name: 'TenOfHearts', value: 1 }, { name: 'TenOfDiamonds', value: 1 }, { name: 'TenOfSpades', value: 1 }, { name: 'TenOfClubs', value: 1 },
-{ name: 'NineOfHearts', value: 0 }, { name: 'NineOfDiamonds', value: 0 }, { name: 'NineOfSpades', value: 0 }, { name: 'NineOfClubs', value: 0 },
-];
+const cardsDeck : card[] = cardsDeckJSON;
 const playerDeck = ref<card[]>([]);
 const bot1Deck = ref<card[]>([]);
 const bot2Deck = ref<card[]>([]);
 const bot3Deck = ref<card[]>([]);
-onMounted(() => {
-  const decks = [playerDeck, bot1Deck, bot2Deck, bot3Deck];
+const mainDeck = ref<card[]>([]);
+const decks = [playerDeck, bot1Deck, bot2Deck, bot3Deck];
+mainDeck.value.push({ name: 'koszulka', value: -1 });
+
+const wait = (ms: number) => new Promise<void>(resolve => setTimeout(resolve, ms));
+
+onMounted(() => { // rozdanie kart
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 6; j++) {
       let rand = Math.floor(Math.random() * cardsDeck.length)
       if (cardsDeck[rand]) decks[i]?.value.push(cardsDeck[rand])
       cardsDeck.splice(rand, 1);
     }
+    decks[i]!.value = sortDeck(decks[i]?.value!)
   }
+  for (let j = 0; j < playerDeck.value.length; j++) { // czy gracz posiada 9 kier
+    if (playerDeck.value[j]?.name == 'NineOfHearts') return
+  }
+  startBotPlay();
 })
+const playCard = (playingCard: card, index: number) => { // rzucenie karty
+  let deckLength = mainDeck.value.length;
+  if (mainDeck.value[0]?.value == -1) { // poczatek gry
+    if (playingCard.name == 'NineOfHearts') {
+      mainDeck.value[0] = playingCard
+      playerDeck.value.splice(index, 1);
+      startBotPlay();
+    } else {
+      startBotPlay();
+    }
+  }
+  else { // normalny rzut
+    if (playingCard.value >= mainDeck.value[deckLength - 1]!.value) {
+      mainDeck.value.push(playingCard)
+      playerDeck.value.splice(index, 1);
+      startBotPlay();
+    }
+  }
+}
+const sortDeck = (deck: card[]) => { // bubble sort talii
+  for (let i = 0; i < deck.length - 1; i++) {
+    for (let j = 0; j < (deck.length - i - 1); j++) {
+      if (deck[j]!.value <= deck[j + 1]!.value) {
+        let temp = deck[j]!;
+        deck[j] = deck[j + 1]!;
+        deck[j + 1] = temp;
+      }
+    }
+  }
+  return deck;
+}
+async function startBotPlay() { // kolej botow
+  for (let i = 1; i < 4; i++) {
+    if (decks[i]?.value.length! > 0) {
+      await wait(1000);
+      const result = botPlay(decks[i]?.value!);
+      if (result) decks[i]!.value = result;
+    }
+  }
+  if(playerDeck.value.length == 0) startBotPlay();
+}
+const botPlay = (deck: card[]) => { // zagranie bota
+  let deckLength = mainDeck.value.length;
+  for (let i = deck.length - 1; i >= 0; i--) {
+    if (mainDeck.value[0]?.value == -1) { // poczatek gry
+      for (let j = 0; j < deck.length; j++) {
+        if (deck[j]?.name == 'NineOfHearts') {
+          mainDeck.value[0] = deck[j]!
+          deck.splice(j, 1);
+          return deck;
+        }
+      }
+      return deck;
+    }
+    else if (deck[i]?.value! >= mainDeck.value[deckLength - 1]?.value!) { // normalna gra
+      mainDeck.value.push(deck[i]!);
+      deck.splice(i, 1);
+      deck = sortDeck(deck);
+      return deck;
+    }
+  }
+  return take3cards(deck, false) // jak nie moze zagrac
+}
+const take3cards = (deck: card[], playerPlay: boolean) => { // wziecie 3 kart z talii
+  let deckLength = mainDeck.value.length;
+  for (let i = deckLength - 1; i > deckLength - 4; i--) {
+    if (mainDeck.value[i]?.name == 'NineOfHearts') return
+    deck.push(mainDeck.value[i]!);
+    mainDeck.value.splice(i, 1);
+  }
+  deck = sortDeck(deck);
+  if (playerPlay == true) startBotPlay();
+  return deck;
+}
 </script>
 
 <template>
   <div id="board">
     <div id="first" class="cards horizontal">
-      <CardComp v-for="card in bot1Deck" :card="card.name" />
+      <CardComp v-for="card in bot1Deck" :card="card.name" :isPlayer="false" />
     </div>
     <div class="cards vertical">
-      <CardComp v-for="card in bot2Deck" :card="card.name" />
+      <CardComp v-for="card in bot2Deck" :card="card.name" :isPlayer="false" />
     </div>
-    <div class="cards">
-      <img src="./assets/Cards/koszulka.png"></img>
+    <div class="cards mainDeck">
+      <img :src="`http://localhost:5173/src/assets/Cards/${c.name}.png`" v-for="c in mainDeck.slice(-3)"></img>
     </div>
     <div class="cards vertical">
-      <CardComp v-for="card in bot3Deck" :card="card.name" />
+      <CardComp v-for="card in bot3Deck" :card="card.name" :isPlayer="false" />
     </div>
     <div id="first" class="cards horizontal">
-      <CardComp v-for="card in playerDeck" :card="card.name" />
+      <CardComp v-for="(card, i) in playerDeck" :card="card.name" :isPlayer="true" @click="playCard(card, i)" />
+      <button @click="take3cards(playerDeck, true)">Take 3 cards</button>
     </div>
   </div>
 
@@ -70,6 +150,10 @@ onMounted(() => {
   display: flex;
   justify-content: center;
   align-items: center;
+}
+
+.mainDeck img {
+  margin-left: -40px;
 }
 
 .cards.horizontal img {
--- src/components/CardComp.vue
+++ src/components/CardComp.vue
@@ -1,24 +1,29 @@
 <script setup lang="ts">
-import { computed } from 'vue';
-const props = defineProps(['card'])
+import { computed, onMounted } from 'vue';
+const props = defineProps(['card', 'isPlayer'])
 const cardType = computed(() => {
-  if (!props.card) return '';
-  return new URL(`../assets/Cards/${props.card}.png`, import.meta.url).href;
+  if(props.isPlayer){
+    if (!props.card) return '';
+    return new URL(`../assets/Cards/${props.card}.png`, import.meta.url).href;
+  }
+  else return new URL(`../assets/Cards/koszulka.png`, import.meta.url).href;
 });
-// console.log(props.card)
 </script>
 
 <template>
-    <img :src=cardType v-on:click="console.log(props.card)"></img>
+    <img :src=cardType class="playerCards" v-if="props.isPlayer" ></img>
+    <img :src=cardType v-else ></img>
 </template>
 
 <style>
   img{
-    /* height: 10vh; */
     margin: 0;
     padding: 0;
     border: 3px solid black;
     border-radius: 5px;
-    /* aspect-ratio: 3 / 4; */
+    transition: 200ms;
+  }
+  .playerCards:hover{
+    scale: 1.05;
   }
 </style>
